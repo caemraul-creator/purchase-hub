9b064d2d0daf38761d1dac6cbada799ba72c2499
caemraul-creator <caemraul@gmail.com>
2025-12-26 14:09:10 +0700

    Update API_URL to new endpoint

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // config.js
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbz_yk90wC8wCVzywMgN8PnH0X6mcm2csGVAoBfDqw42azQvSEvAMDmsmxqKkggI_rrLtw/exec";
+  "https://script.google.com/macros/s/AKfycbxHi47SLYRrvNenK38RC10sxHNRBS4wYobQm4zpzFYguE5wNiFUmJ2VWt1y3tIOPpColQ/exec";
+
 
 
 
